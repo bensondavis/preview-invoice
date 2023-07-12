7a65654e0e856222bd7991a514866a36ef5a7574
Benson Davis <bensondavis61@gmail.com>
2023-07-12 11:59:22 +0530

    edited pdfBtn

--- src/components/PdfBtn.tsx
+++ src/components/PdfBtn.tsx
@@ -1,7 +1,6 @@
 import Button from "@mui/material/Button";
 import DownloadIcon from "@mui/icons-material/Download";
 import styles from "../styles/PdfBtn.module.css";
-import Link from "next/link";
 
 interface PdfBtnProps {
   link: string;
@@ -9,16 +8,16 @@ interface PdfBtnProps {
 
 const PdfBtn = ({ link }: PdfBtnProps) => {
   return (
-    <Link className={styles.btn} href={link}>
-      <Button
-        startIcon={<DownloadIcon />}
-        variant="outlined"
-        color="primary"
-        size="small"
-      >
-        PDF
-      </Button>
-    </Link>
+    <Button
+      href={link}
+      startIcon={<DownloadIcon />}
+      variant="outlined"
+      color="primary"
+      size="small"
+      className={styles.btn}
+    >
+      PDF
+    </Button>
   );
 };
 
